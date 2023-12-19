e789333fc73b833276cd0ea8a9af2e21516bcf30
DanielKliver <povishevd@mail.ru>
2023-12-19 12:57:06 +0700

    commit 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 
 const int num_of_polygon = 5;
 
@@ -29,66 +30,66 @@ typedef class polygon
 
 }polygon;
 
-typedef class plane 
-{
-	public:
-	polygon* list_of_polygons;
-
-}plane;
-
-
-typedef class BSP_Tree
+class BSP_Tree
 {
 	private:
 
-	plane *BSP_Plane;
-	BSP_Node *root;
-
-	
+	BSP_Node *root = NULL;
+	polygon *list_of_polygons = NULL;
 
 	public:
 
-	void add_node(plane *BSP_Plane, BSP_Node *root)
+	void add_node(polygon *list_of_polygons, BSP_Node *root)
 	{
-
-	}
-
-	BSP_Tree(plane *BSP_Plane)
-	{
-		BSP_Node *root;root = new BSP_Node;this->root = root;
-		this->BSP_Plane = BSP_Plane;
-		for(int i = 0; i<num_of_polygon; i++)
+		//НЕТ РЕКУРСИИ
+		if(root==NULL)
 		{
-			polygon *polygon_now = &BSP_Plane->list_of_polygons[i];
-			if(i == 0)
-			{
-				root->index = i;
-			}
-			make_tree(BSP_Plane, root);
+			//первый полигон из списка будет корнем
+			root = new BSP_Node;
+			root->index = 0;
 		}
-		
+		//НЕТ РЕКУРСИИ
+
+		//проверка расположения оставшихся полигонов, относительно текущего
+		//вычисление размера текущего списка полигонов
+		//int list_size = 0;for(int i: list_of_polygons){list_size++}//for(int i = 0; list_of_polygons<5;i++){list_size++;}
+
+		for(int i = 1; i< num_of_polygon; i++)
+		{
+			cout<<"count"<<endl;
+		}
+		////////////////////////////////////////////////////////////////////
 	}
+
+	BSP_Tree(polygon *list_of_polygons)
+	{
+		this->list_of_polygons = list_of_polygons;
+		add_node(list_of_polygons, root);
+	}
+
 	~BSP_Tree()
 	{
 
 	}
 	
-}BSP_Tree;
+};
 
 int main()
 {
 	//массив полигонов 
-	plane *plane1 = new plane; plane1->list_of_polygons = new polygon[num_of_polygon]
+
+	polygon *list_of_polygons = new polygon[num_of_polygon]
 	{
-		{{3, 12}, {10, 16}},
-		{{10, 14}, {14, 9}},
-		{{9, 3}, {15, 7}},
-		{{15, 12}, {17, 17}},
-		{{5, 3}, {5, 9}}
+	{{3, 12}, {10, 16}},
+	{{10, 14}, {14, 9}},
+	{{9, 3}, {15, 7}},
+	{{15, 12}, {17, 17}},
+	{{5, 3}, {5, 9}}
+
 	};
 
-	BSP_Tree tree1(plane1);
+	BSP_Tree tree1(list_of_polygons);
 
-	delete [] plane1;
+	delete [] list_of_polygons;
 	return 0;
 }
