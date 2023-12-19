90278f61b9c1875277994576b7a871c73f62a1cc
DanielKliver <povishevd@mail.ru>
2023-12-19 19:59:16 +0700

    starting normal realization, added working constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
-#include<vector>
 
-const int num_of_polygon = 5;
 
 using namespace std;
 
@@ -28,6 +26,8 @@ typedef class polygon
 	point one;
 	point two;
 
+	int index_of_polygon;
+
 }polygon;
 
 class BSP_Tree
@@ -39,32 +39,67 @@ class BSP_Tree
 
 	public:
 
+	polygon* del_first_polygon(polygon *list_of_polygons, int num_of_polygon)
+	{
+		polygon *list_of_polygons_new = new polygon[num_of_polygon-1];//размер массива - 1
+
+		//переменная для перебера новго массива
+		int i = 0;
+		//переменная для перебора старого массива
+		int j = 1;
+		while(i<num_of_polygon)
+		{
+			list_of_polygons_new[i] = list_of_polygons[j];
+			i++; j++;
+		}
+
+		return list_of_polygons_new;
+	}
+	/*
 	void add_node(polygon *list_of_polygons, BSP_Node *root)
 	{
 		//НЕТ РЕКУРСИИ
 		if(root==NULL)
 		{
 			//первый полигон из списка будет корнем
-			root = new BSP_Node;
-			root->index = 0;
+			//root = new BSP_Node;
+			//root->index = 0;
+			int Node_now = 0;
 		}
 		//НЕТ РЕКУРСИИ
 
 		//проверка расположения оставшихся полигонов, относительно текущего
 		//вычисление размера текущего списка полигонов
-		//int list_size = 0;for(int i: list_of_polygons){list_size++}//for(int i = 0; list_of_polygons<5;i++){list_size++;}
-
 		for(int i = 1; i< num_of_polygon; i++)
 		{
-			cout<<"count"<<endl;
+			//if(list_of_polygons[Node_now]->one->x<list_of_polygons[i]->one->x)
+			//{
+
+			//}
 		}
 		////////////////////////////////////////////////////////////////////
 	}
+	*/
 
-	BSP_Tree(polygon *list_of_polygons)
+	void Build_BSP_Tree(polygon* list_of_polygons, BSP_Node* root)
+	{
+
+	}
+	BSP_Tree(polygon *list_of_polygons, int num_of_polygon)
 	{
 		this->list_of_polygons = list_of_polygons;
-		add_node(list_of_polygons, root);
+		if(list_of_polygons != NULL)
+		{
+			//если список полигоонов не пуст, 
+			//корнем делаем полигон с индексом 0 (1)
+			root = new BSP_Node;
+			root->index = list_of_polygons[0].index_of_polygon;
+
+			//функция удаления первого элемента листа полигонов
+			this->list_of_polygons = del_first_polygon(list_of_polygons, num_of_polygon);
+		}
+		Build_BSP_Tree(list_of_polygons, root);
+		//     add_node(list_of_polygons, root);
 	}
 
 	~BSP_Tree()
@@ -77,18 +112,18 @@ class BSP_Tree
 int main()
 {
 	//массив полигонов 
-
+	int num_of_polygon = 5;
 	polygon *list_of_polygons = new polygon[num_of_polygon]
 	{
-	{{3, 12}, {10, 16}},
-	{{10, 14}, {14, 9}},
-	{{9, 3}, {15, 7}},
-	{{15, 12}, {17, 17}},
-	{{5, 3}, {5, 9}}
+	{{3, 12}, {10, 16}, 1},
+	{{10, 14}, {14, 9}, 2},
+	{{9, 3}, {15, 7}, 3},
+	{{15, 12}, {17, 4}},
+	{{5, 3}, {5, 9}, 5}
 
 	};
 
-	BSP_Tree tree1(list_of_polygons);
+	BSP_Tree tree1(list_of_polygons, num_of_polygon);
 
 	delete [] list_of_polygons;
 	return 0;
